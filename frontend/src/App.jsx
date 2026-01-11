6b52237f743a1a65eeaf71a8236b4cd35b52fd5e
Aziz Tazrout <aztazrout@gmail.com>
2026-01-11 13:49:55 -0500

    Finalize frontend setup, configuration, and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PublicLayout from "./components/layout/PublicLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Landing from "./pages/public/Landing";
+import HotelDetails from "./pages/public/HotelDetails";
+import SearchRooms from "./pages/public/SearchRooms";
+import RoomDetails from "./pages/public/RoomDetails";
+import BookRoom from "./pages/public/BookRoom";
+import HotelChains from "./pages/public/HotelChains.jsx";
+import Hotels from "./pages/public/Hotels.jsx";
+import ReservationConfirmation from "./pages/public/ReservationConfirmation.jsx";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PublicLayout />}>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/hotels" element={<Hotels />} />
+                    <Route path="/hotels/:id" element={<HotelDetails />} />
+                    <Route path="/search" element={<SearchRooms />} />
+                    <Route path="/rooms/:roomId" element={<RoomDetails />} />
+                    <Route path="/book/:roomId" element={<BookRoom />} />
+                    <Route path="/hotel-chains" element={<HotelChains />} />
+                    <Route path="/reservations/:id/confirmation" element={<ReservationConfirmation />} />
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App
